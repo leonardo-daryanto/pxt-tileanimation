7e2d6d0e5df0552cfb693629089b1a2c6b37cbfd
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-08-27 11:09:17 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+namespace tileAnimation {
+    /**
+     *  Make a ani tile-Anim where put the animation
+     */
+    //% block= set tile col $col row $row animate $anim
+    //% blockId=tileanimation
+    export function setTileAnimateAt(col: number, row: number, anim: Image[], imageInto: Image): void {
+        
+        let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+        
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(col, row))
+        
+        animation.runImageAnimation(mySprite, anim, 100, false)
+        
+        tiles.setTileAt(tiles.getTileLocation(col, row), imageInto) 
+    }
+}
