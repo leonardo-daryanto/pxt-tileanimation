0b64ad210628a00d9f75792dc453b216377ca059
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-08-27 11:15:22 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,9 @@ namespace tileAnimation {
     /**
      *  Make a ani tile-Anim where put the animation
      */
-    //% block= set tile col $col row $row animate $anim
-    //% blockId=tileanimation
+    //% block= "set tile col $col row $row animate $anim"
+    //% blockId=tileanimationsettileanimateat
+    
     export function setTileAnimateAt(col: number, row: number, anim: Image[], imageInto: Image): void {
         
         let mySprite = sprites.create(img`
